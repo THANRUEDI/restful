d4107ede1d4c4de6f6a249db26dd94bf27107e37
heromawin11 <heromawin112@gmail.com>
2024-11-29 13:00:19 +0700

    first commit

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,10 @@
+// functions ที่ทำงานตาม route
+exports.get = (req,res)=>{
+    res.send("Get Kruphakpoom here");
+}
+exports.getById =(req, res)=>{
+    res.send("Get products " + req.params.id);
+}
+exports.create = (req, res)=>{
+    res.send("Create a product");
+  }
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+// get port number from enviroment settings
+require('dotenv').config({path:'./config.env'});
+const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const productRoute = require('./routes/product.route');
+
+// cross origin resource sharing
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// path url
+// app.[method]();
+
+// match GET localhost:4000/
+app.get("/",(req,res)=>{
+    res.send("Kruchanon");
+});
+//ใช้ productRoute เมื่อ request ขึ้นต้นด้วย /products
+app.use("/products", productRoute);
+
+app.listen(port, () => {
+  console.log("App started at port: " + port);
+});
